94558383b4304f7a730791d4aedf4c8fd9b037e5
tzu lin chang <coooo77@hotmail.com>
2022-12-14 23:07:26 +0800

    fix: baseUrl error

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   url: 'https://coooo77.github.io/',
-  baseUrl: '/',
+  baseUrl: '/tech-blog/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
